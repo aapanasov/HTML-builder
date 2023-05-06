e0c4ce7dbf7f8980d82390dda1771850d903fa94
bekinblack <aapanasov@gmail.com>
2023-05-06 16:10:53 +0300

    feat: complete task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,25 @@
+const fs  = require('fs/promises');
+const path = require('path');
+
+const folder = path.join(__dirname, 'secret-folder');
+
+const getFiles = async () => {
+  try {
+    const files = await fs.readdir(folder);
+
+    files.forEach(async file => {
+      const filePath = path.join(folder, file);
+      const fileStat = await fs.stat(filePath);
+
+      if(fileStat.isFile()){
+        const name = path.parse(filePath).name;
+        console.log(`${name} - ${path.extname(filePath).slice(1)} - ${fileStat.size/1000}kb` );
+      }
+    });
+
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+getFiles();
