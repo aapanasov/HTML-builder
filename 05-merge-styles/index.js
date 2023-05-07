3af56be696f5b4092d8debd219f211f76aac95c3
bekinblack <aapanasov@gmail.com>
2023-05-07 10:33:55 +0300

    feat: complete task 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const fs = require('fs/promises');
+
+const mergeSyles = async (src, bundle) => {
+  const filesInFolder = [];
+
+  try {
+    const files = await fs.readdir(src);
+
+    for (const file of files){
+      const filePath = path.join(src, file);
+      const fileStat = await fs.stat(filePath);
+
+      if (fileStat.isFile() && path.extname(filePath) === '.css') {
+        const fileContent = await fs.readFile(filePath, 'utf-8');
+        filesInFolder.push(fileContent);
+      }
+    }
+
+    const content = filesInFolder.join('\n');
+    await fs.writeFile(bundle, content);
+
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+const src = path.join(__dirname, 'styles');
+const bundle = path.join(__dirname,'project-dist', 'bundle.css');
+
+mergeSyles(src, bundle);
