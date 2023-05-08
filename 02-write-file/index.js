65a41ba06fd6b9be25442498b3d81d31cb623ae5
bekinblack <aapanasov@gmail.com>
2023-05-08 11:01:46 +0300

    fix: close on type "exit"

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,14 +1,19 @@
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
 
-const output = fs.createWriteStream(path.join(__dirname, 'output.txt'));
-output.on('error',(err) => console.log('Error:', err));
+const output = fs.WriteStream(path.join(__dirname, 'output.txt'));
+const readLine = require('readline').createInterface(process.stdin, process.stdout);
 
-console.log('Enter some text:');
-
-process.stdin.pipe(output);
-
-process.on('SIGINT',  ()=>{
-  console.log('Goodbye!');
-  process.exit();
-});
+console.log('Enter some text');
+readLine.setPrompt('> ');
+readLine.prompt();
+readLine
+  .on('line', line => {
+    if (line === 'exit') readLine.close();
+    output.write(line + '\n');
+    readLine.prompt();
+  })
+  .on('close', () => {
+    console.log('\nGoodbye!');
+    process.exit(0);
+  });
